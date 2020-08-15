c5f1e40ecfbe0dc84b55bc0d1001211901c38ecc
Khushi Kapoor <thekhushikapoor@gmail.com>
2020-08-15 19:20:53 +0530

    header and footer added.

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Card, CardImg, CardImgOverlay,CardTitle} from 'reactstrap';
-import DishdetailComponent from './DishdetailComponent'
 
 function RenderMenuItem({dish, onClick}){
     return(
